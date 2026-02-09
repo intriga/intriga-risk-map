dce29a4e3db43760e38264f655b2c8f04723db27
intriga <intriga2402@gmail.com>
2026-02-09 08:47:08 -0400

    -- coment

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let vulnerabilities = [];
 let riskChart, riskDistributionChart, owaspDistributionChart;
 let currentTheme = 'light';
 
-// Categorías OWASP TOP 10 2021
+// Categorías OWASP TOP 10 2021 --
 const owaspCategories = [
     "A01:2021 - Broken Access Control",
     "A02:2021 - Cryptographic Failures", 
